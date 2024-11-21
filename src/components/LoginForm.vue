c6fdd00dd9c875d6da85d94553a4d0f596ce4719
EnzoDelmachio <enzo.dlmc@gmail.com>
2024-11-21 16:08:01 -0300

    :lipstick: feat: login page

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="flex h-screen items-center justify-center bg-gray-100">
-        <div class="bg-white p-8 rounded shadow-md">
-            <h2 class="text-2xl font-bold mb-6">Login</h2>
-            <form>
+    <div class="min-h-screen flex items-center justify-center w-full bg-gray-100">
+        <div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
+            <h1 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Bem-vindo</h1>
+            <form action="#">
                 <div class="mb-4">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-                        Email
-                    </label>
+                    <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Endere;o de email</label>
                     <input type="email" id="email"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Seu email">
+                        class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        placeholder="seu@email.com" required>
                 </div>
-                <div class="mb-6">
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-                        Senha
-                    </label>
+                <div class="mb-4">
+                    <label for="password"
+                        class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Senha</label>
                     <input type="password" id="password"
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        placeholder="Sua senha">
+                        class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+                        placeholder="Insira sua senha" required>
+                    <a href="#"
+                        class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Esqueci minha senha</a>
                 </div>
-                <div class="flex items-center justify-between">
-                    <RouterLink to="/livros"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                        Entrar
-                    </RouterLink>
-                    <a href="#" class="inline-block align-baseline text-sm text-blue-500 hover:text-blue-800">
-                        Esqueceu a senha?
-                    </a>
+                <div class="flex items-center justify-between mb-4">
+                    <div class="flex items-center">
+                        <input type="checkbox" id="remember"
+                            class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none"
+                            checked>
+                        <label for="remember" class="ml-2 block text-sm text-gray-700 dark:text-gray-300">Lembrar-me</label>
+                    </div>
+                    <a href="#"
+                        class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Criar conta</a>
                 </div>
+                <RouterLink to="/livros" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Login
+                </RouterLink>
             </form>
         </div>
     </div>
